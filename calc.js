b9e9538fdcff8c0fb64e13644cb0eddfc729aefe
Thaelyn <michael.c.legrand@gmail.com>
2017-07-04 13:46:51 +0200

    Retrait du Number() lors de la déclaration de la 3ème variable

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@ var maVar1 = Number(prompt("Veuillez saisir votre première variable"));
 
 var maVar2 = Number(prompt("Veuillez saisir votre deuxième variable"));
 
-result = Number(maVar2+maVar1);
+result = maVar2+maVar1;
 
 alert("Voici le résultat de votre saisie" + " " + maVar1 + "+" + maVar2 + " " + "=" + " " + result);
 
