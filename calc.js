e02449822866713bb93e6c4f64d870669573b226
Thaelyn <michael.c.legrand@gmail.com>
2017-07-04 12:35:18 +0200

    Codage du fichier calc.js

---
+++ calc.js
@@ -0,0 +1,9 @@
+var maVar1 = Number(prompt("Veuillez saisir votre première variable"));
+
+var maVar2 = Number(prompt("Veuillez saisir votre deuxième variable"));
+
+result = Number(maVar2+maVar1);
+
+alert("Voici le résultat de votre saisie" + " " + maVar1 + "+" + maVar2 + " " + "=" + " " + result);
+
+alert("Voici le résultat de votre saisie" + " " + maVar1 + "*" + maVar2 + " " + "=" + " " + maVar2*maVar1);
